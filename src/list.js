d6826a9540ce09c9582d7074ee3a70024025911f
dearmcw <dearmcw0070@gmail.com>
2019-03-07 13:48:40 +0900

    리스트 목록 작성완료

---
+++ src/list.js
@@ -0,0 +1,9 @@
+const list = [
+    { id: 1, title: '도서하기', description: '상세1', done: false },
+    { id: 2, title: '씻기', description: '상세2', done: false },
+    { id: 3, title: '밥먹기', description: '상세3', done: false },
+    { id: 4, title: '놀러가기', description: '상세4', done: false },
+    { id: 5, title: '고양이밥주기', description: '상세5', done: true },
+]
+
+export default list;
